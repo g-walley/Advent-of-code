32c6411430169285e2d429455611be064b61db8e
Gareth Walley <gareth@walley.me>
2022-12-02 06:54:14 +0000

    Test failing: day02 example

---
+++ 2022/day02/solution.py
@@ -0,0 +1,5 @@
+
+
+def pt1(raw_data: str):
+    """Part 1"""
+    print(raw_data)
---
+++ 2022/day02/test_solution.py
@@ -0,0 +1,38 @@
+"""Tests for solution"""
+
+from pathlib import Path
+from shared_test import AOCTest
+from day02.solution import pt1
+
+class TestCase(AOCTest):
+    """Test case for Day 1"""
+    ex = "2022/day02/example.txt"
+    in_path = "2022/day02/input.txt"
+
+    def test_example_part_1(self):
+        """Input example data to part 1"""
+        self.assertEqual(
+            pt1(Path(self.ex).read_text(encoding="utf8")),
+            15,
+        )
+
+    # def test_example_part_2(self):
+    #     """Input example data to part 2"""
+    #     self.assertEqual(
+    #         pt2(Path(self.ex).read_text(encoding="utf8")),
+    #         45000,
+    #     )
+
+    def test_input_part_1(self):
+        """Test Created with answer to allow for refactoring."""
+        self.assertEqual(
+            pt1(Path(self.in_path).read_text(encoding="utf8")),
+            74394,
+        )
+
+    # def test_input_part_2(self):
+    #     """Test Created with answer to allow refactoring"""
+    #     self.assertEqual(
+    #         pt2(Path(self.in_path).read_text(encoding="utf8"), 3),
+    #         212836,
+    #     )
