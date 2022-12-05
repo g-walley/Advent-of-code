7c8ce5203bae4d7596a5d59794a9ca6f24dd98fc
Gareth Walley <gareth@walley.me>
2022-12-05 22:31:31 +0000

    Refactored to group code in funcs

--- 2022/day05/solution.py
+++ 2022/day05/solution.py
@@ -1,66 +1,54 @@
 from collections import deque
 import re
-from typing import Dict, Deque, Iterable
+from typing import Callable, Dict, Deque, Iterable
 
 
-def build_stacks(in_crates: str):
+def build_stacks(in_crates: str)-> Dict[str, Deque]:
     """Builds the crate stack dictionary"""
     in_crates: Iterable[str] = in_crates.splitlines()
     in_crates.reverse()
 
     # create a dictionary mapping of positions to column names
-    col_positions = {
-        p: col
-        for p, col in enumerate(in_crates.pop(0))
-        if col != ' '
-    }
+    col_positions = {p: col for p, col in enumerate(in_crates.pop(0)) if col != ' '}
+
     # initialise deque objects
-    stacks : Dict[str, Deque] = {
-        col: deque()
-        for col in col_positions.values()
-    }
+    stacks : Dict[str, Deque] = {col: deque() for col in col_positions.values()}
 
     for level in in_crates:
         for p, col in enumerate(level):
-            char_num = ord(col)
-            if 65 < char_num and char_num < 91:
+            if p in col_positions and col != ' ':
                 stacks[col_positions[p]].append(col)
 
     return stacks
 
-def pt1(in_crates: str, in_instr: str):
-    """part 1"""
-    stacks = build_stacks(in_crates)
-
-    # move crates between stacks:
+def move_crates(stacks: Dict[str, Deque], instructions: str, crane: Callable):
+        # move crates between stacks:
     re_instr = re.compile(r'move (?P<move>\d+) from (?P<from>\d+) to (?P<to>\d+)')
 
-    for line in in_instr.splitlines():
+    for line in instructions.splitlines():
         instr: Dict = re_instr.search(line).groupdict()
+        crane(stacks, instr)
+
+def pt1(in_crates: str, in_instr: str):
+    """part 1"""
+
+    def pt1_crane(stacks: Dict[str, Deque], instr: Dict):
         for _ in range(int(instr['move'])):
             crate = stacks[instr['from']].pop()
             stacks[instr['to']].append(crate)
 
-    return ''.join([
-        stack.pop()
-        for stack in stacks.values()
-    ])
+    move_crates(stacks := build_stacks(in_crates), in_instr, pt1_crane)
+
+    return ''.join([stack.pop() for stack in stacks.values()])
 
 def pt2(in_crates: str, in_instr: str):
     """part 2"""
-    stacks = build_stacks(in_crates)
-    crane = deque()
-    # move crates between stacks:
-    re_instr = re.compile(r'move (?P<move>\d+) from (?P<from>\d+) to (?P<to>\d+)')
-    for line in in_instr.splitlines():
-        instr: Dict = re_instr.search(line).groupdict()
+    def pt2_crane(stacks: Dict[str, Deque], instr: Dict):
+        crane_grabber = deque()
         for _ in range(int(instr['move'])):
-            crane.appendleft(stacks[instr['from']].pop())
-        while crane:
-            stacks[instr['to']].append(crane.popleft())
+            crane_grabber.appendleft(stacks[instr['from']].pop())
+        while crane_grabber:
+            stacks[instr['to']].append(crane_grabber.popleft())
 
-    return ''.join([
-        stack.pop()
-        for stack in stacks.values()
-    ])
-
+    move_crates(stacks := build_stacks(in_crates), in_instr, pt2_crane)
+    return ''.join([stack.pop() for stack in stacks.values()])
