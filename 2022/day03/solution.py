b2eefd9beb0eb2319b008d38e161430e559061a4
Gareth Walley <gareth@walley.me>
2022-12-05 13:13:54 +0000

    minor update to day03

--- 2022/day03/solution.py
+++ 2022/day03/solution.py
@@ -1,21 +1,12 @@
+import string
 
-letter_prio = {
-    'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6,
-    'g': 7, 'h': 8, 'i': 9, 'j': 10,'k': 11,'l': 12,
-    'm': 13,'n': 14,'o': 15,'p': 16,'q': 17,'r': 18,
-    's': 19,'t': 20,'u': 21,'v': 22,'w': 23,'x': 24,
-    'y': 25,'z': 26,'A': 27,'B': 28,'C': 29,'D': 30,
-    'E': 31,'F': 32,'G': 33,'H': 34,'I': 35,'J': 36,
-    'K': 37,'L': 38,'M': 39,'N': 40,'O': 41,'P': 42,
-    'Q': 43,'R': 44,'S': 45,'T': 46,'U': 47,'V': 48,
-    'W': 49,'X': 50,'Y': 51,'Z': 52,
-}
+letter_prio = {l: idx+1 for idx, l in  enumerate(string.ascii_letters)}
 
 def pt1(raw_input: str):
     """comparing two halfs of each string"""
     shared_items = []
     for backpack in raw_input.splitlines():
-        half = int(len(backpack)/2)
+        half = len(backpack)//2
         comp_1 = backpack[:half]
         comp_2 = backpack[half:]
 
