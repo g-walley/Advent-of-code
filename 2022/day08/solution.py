7b9483f717ab55289c03ec8b243230cb58ca4020
Gareth Walley <gareth@walley.me>
2022-12-08 18:28:39 +0000

    This is a proper solution

--- 2022/day08/solution.py
+++ 2022/day08/solution.py
@@ -1,143 +1,140 @@
+from __future__ import annotations
 from pathlib import Path
-import pandas as pd
+
+from dataclasses import dataclass
+from typing import Dict
+
+
+@dataclass(frozen=True)
+class Coordinate:
+    x: int
+    y: int
+
+    def __add__(self, other: Coordinate) -> Coordinate:
+        return Coordinate(self.x + other.x, self.y + other.y)
+
+
+@dataclass(frozen=True)
+class Direction(Coordinate):
+    @property
+    def length(self):
+        if self.x == 0:
+            return self.y
+        if self.y == 0:
+            return self.x
+        assert False, "Uni-dimensional directions only!"
+
+
+directions = {
+    Direction(0, -1),
+    Direction(0, 1),
+    Direction(1, 0),
+    Direction(-1, 0),
+}
+
+
+@dataclass
+class Rectangle:
+    width: int
+    length: int
+
+    def __post_init__(self):
+        assert self.width > 0
+        assert self.length > 0
+
+
+@dataclass
+class Tree:
+    height: int
+
+
+@dataclass
+class Forest:
+    trees: Dict[Coordinate, Tree]  # 🎄
+    size: Rectangle
+
+
+def plant_forest(input_path: Path):
+    file_content = input_path.read_text()
+
+    trees = {}
+    for y, row in enumerate(file_content.splitlines()):
+        for x, height in enumerate(row.strip()):
+            trees[Coordinate(x, y)] = Tree(int(height))
+
+    return Forest(trees, size=Rectangle(x + 1, y + 1))
+
+
+@dataclass
+class Ray:
+    """A point that moves in a direction at a height, and records distance travelled."""
+
+    position: Coordinate
+    height: int
+    direction: Direction
+    dist: int = 0
+    hit: bool = False
+
+    def __post_init__(self):
+        assert self.height >= 0
+        assert not self.direction.x or not self.direction.y
+
+
+def trace_ray(forest: Forest, ray: Ray) -> Ray:
+    # extend ray in a straight line
+    ray.position = ray.position + ray.direction
+    # record distance flown
+    ray.dist = ray.dist + abs(ray.direction.length)
+
+    # Did we hit a 🧱?
+    out_of_bounds_x = (
+        ray.position.x < 0 or forest.size.width <= ray.position.x
+        if ray.direction.x != 0
+        else False
+    )
+    out_of_bounds_y = (
+        ray.position.y < 0 or forest.size.length <= ray.position.y
+        if ray.direction.y != 0
+        else False
+    )
+    if out_of_bounds_x or out_of_bounds_y:
+        ray.dist -= 1
+        return ray
+
+    # Did it hit a 🎄?
+    ray.hit = forest.trees[ray.position].height >= ray.height
+    if ray.hit:
+        return ray
+    return trace_ray(forest=forest, ray=ray)
 
 
 def pt1(input_path: Path):
     """part 1"""
-    forest: pd.DataFrame = pd.read_csv(input_path, delim_whitespace=True, header=None)
+    forest = plant_forest(input_path)
+    visible_trees = 0
 
-    middle_trees = forest.iloc[1:-1, 1:-1]
-    visible_trees = forest.size - middle_trees.size
-    for row in range(1, forest.shape[1] - 1):
-        for col in range(1, forest.shape[1] - 1):
-            tree_height = forest.iat[row, col]
-
-            trees_to_left = forest.iloc[row, 0:col]
-            if max(trees_to_left) < tree_height:
+    for position, tree in forest.trees.items():
+        for dir in directions:
+            ray = trace_ray(forest=forest, ray=Ray(position, tree.height, dir))
+            if not ray.hit:
                 visible_trees += 1
-                continue
+                break
 
-            trees_to_right = forest.iloc[row, col + 1 :]
-            if max(trees_to_right) < tree_height:
-                visible_trees += 1
-                continue
-
-            trees_above = forest.iloc[0:row, col]
-            if max(trees_above) < tree_height:
-                visible_trees += 1
-                continue
-
-            trees_below = forest.iloc[row + 1 :, col]
-            if max(trees_below) < tree_height:
-                visible_trees += 1
-                continue
     return visible_trees
 
 
-def pt2(input_path):
+def pt2(input_path: Path):
     """part 2"""
-    forest: pd.DataFrame = pd.read_csv(input_path, delim_whitespace=True, header=None)
+    forest = plant_forest(input_path)
 
     max_score = 0
-    for row in range(1, forest.shape[0] - 1):
-        for col in range(1, forest.shape[1] - 1):
-            tree_height = forest.iat[row, col]
-            right_score = 0
-            left_score = 0
-            above_score = 0
-            below_score = 0
+    for position, tree in forest.trees.items():
+        score = 1
+        for dir in directions:
+            ray = trace_ray(forest=forest, ray=Ray(position, tree.height, dir))
+            score *= ray.dist
 
-            trees_to_left = forest.iloc[row, 0:col]
-            max_tree_height = trees_to_left.max()
-            if max_tree_height < tree_height:
-                left_score = trees_to_left.size
-            elif trees_to_left.min() >= tree_height:
-                left_score = 1
-            else:
-                max_tree_loc = trees_to_left.idxmax()
-                while True:
-                    trees_to_left = forest.iloc[row, max_tree_loc + 1 : col]
-                    if trees_to_left.size < 1:
-                        left_score = abs(max_tree_loc - col)
-                        break
-                    m_height = trees_to_left.max()
-                    if m_height < tree_height:
-                        left_score = abs(max_tree_loc - col)
-                        break
-                    max_tree_loc = trees_to_left.idxmax()
-                    left_score = abs(max_tree_loc - col)
-
-            trees_to_right = forest.iloc[row, col + 1 :]
-            max_tree_height = trees_to_right.max()
-            if max_tree_height < tree_height:
-                right_score = trees_to_right.size
-            elif trees_to_right.min() >= tree_height:
-                right_score = 1
-            else:
-                max_tree_loc = trees_to_right.idxmax()
-                while True:
-                    trees_to_right = forest.iloc[row, col + 1 : max_tree_loc]
-                    if trees_to_right.size < 1:
-                        right_score = abs(max_tree_loc - col)
-                        break
-                    m_height = trees_to_right.max()
-
-                    if m_height < tree_height:
-                        right_score = abs(max_tree_loc - col)
-                        break
-                    max_tree_loc = trees_to_right.idxmax()
-                    right_score = abs(max_tree_loc - col)
-
-            trees_above = forest.iloc[0:row, col]
-            max_tree_height = trees_above.max()
-            if max_tree_height < tree_height:
-                above_score = trees_above.size
-            elif trees_above.min() >= tree_height:
-                above_score = 1
-            else:
-                max_tree_loc = trees_above.idxmax()
-                while True:
-                    trees_above = forest.iloc[max_tree_loc + 1 : row, col]
-                    if trees_above.size < 1:
-                        above_score = abs(max_tree_loc - row)
-                        break
-                    m_height = trees_above.max()
-
-                    if m_height < tree_height:
-                        above_score = abs(max_tree_loc - row)
-                        break
-
-                    max_tree_loc = trees_above.idxmax()
-                    above_score = abs(max_tree_loc - row)
-
-            trees_below = forest.iloc[row + 1 :, col]
-            max_tree_height = trees_below.max()
-            if max_tree_height < tree_height:
-                below_score = trees_below.size
-            elif trees_below.min() >= tree_height:
-                below_score = 1
-            else:
-                max_tree_loc = trees_below.idxmax()
-                while True:
-                    trees_below = forest.iloc[row + 1 : max_tree_loc, col]
-                    if trees_below.size < 1:
-                        below_score = abs(max_tree_loc - row)
-                        break
-                    m_height = trees_below.max()
-
-                    if m_height < tree_height:
-                        below_score = abs(max_tree_loc - row)
-                        break
-
-                    max_tree_loc = trees_below.idxmax()
-                    below_score = abs(max_tree_loc - row)
-
-            score = left_score * right_score * above_score * below_score
-
-            assert all(
-                [above_score, below_score, right_score, left_score]
-            ), f"{above_score, below_score, right_score, left_score}"
-            if score > max_score:
-                max_score = score
+        if score > max_score:
+            max_score = score
 
     return max_score
