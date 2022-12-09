8e19467f35b69b1de811d186d88b4008839c9ade
Gareth Walley <gareth@walley.me>
2022-12-09 16:17:29 +0000

    day09 Part 2

--- 2022/day09/solution.py
+++ 2022/day09/solution.py
@@ -1,43 +1,150 @@
 from __future__ import annotations
+from collections import deque
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
+from typing import Deque, Set
+from parse import *
+import sys
+
+gettrace = getattr(sys, "gettrace", None)
 
 
-@dataclass
+@dataclass(frozen=True)
 class Vector2D:
     x: int
     y: int
 
-    def __sub__(self, other):
+    def __sub__(self, other: Vector2D):
         return Vector2D(self.x - other.x, self.y - other.y)
 
-    def __add__(self, other):
-        return Vector2D(self.x + other.x, self.y + other.y)
-
 
 @dataclass
-class Head(Vector2D):
+class Knot:
+    child: Knot | None
+    position: Vector2D = Vector2D(0, 0)
+    visited: Set[Vector2D] = field(default_factory=set)
+
+    def __post_init__(self):
+        self.visited.add(self.position)
+
+    def touching(self, other: Knot):
+        return (
+            max(
+                abs(self.position.x - other.position.x),
+                abs(self.position.y - other.position.y),
+            )
+            <= 1
+        )
+
+    def delta(self, other: Knot):
+        return Vector2D(
+            (self.position.x - other.position.x)
+            // (abs(self.position.x - other.position.x) or 1),
+            (self.position.y - other.position.y)
+            // (abs(self.position.y - other.position.y) or 1),
+        )
+
+    def move(self, direction: Vector2D):
+        self.position = Vector2D(
+            self.position.x + direction.x,
+            self.position.y + direction.y,
+        )
+        if self.child:
+            if not self.touching(self.child):
+                self.child.move(direction=self.delta(self.child))
+        else:
+            self.visited.add(Vector2D(self.position.x, self.position.y))
+
+    def cells_visited(self) -> int:
+        if self.child:
+            return self.child.cells_visited()
+        return self.visited
 
 
-
-@dataclass
-class Tail(Vector2D):
-    ...
-
-directions = {
-    "R": Vector2D(1,  0);
-    "L": Vector2D(-1, 0);
-    "U": Vector2D(0,  1);
-    "D": Vector2D(0, -1);
+direction_vectors = {
+    "R": Vector2D(1, 0),
+    "L": Vector2D(-1, 0),
+    "U": Vector2D(0, 1),
+    "D": Vector2D(0, -1),
 }
+
+
+def decode_instructions(file_path: Path) -> Deque[Vector2D]:
+    instructions: Deque[Vector2D] = deque()
+    for line in file_path.read_text().splitlines():
+        input = parse("{direction} {distance}", line.strip()).named
+        direction = direction_vectors[input["direction"]]
+        distance = int(input["distance"])
+        for _ in range(distance):
+            instructions.append(direction)
+
+    return instructions
+
+
+def construct_rope(length):
+    head = Knot(child=None)
+    current_knot = head
+    for _ in range(length - 1):
+        current_knot.child = Knot(child=None)
+        current_knot = current_knot.child
+    return head
+
 
 def pt1(path: Path):
     """part 1"""
 
-    head = Head(0, 0)
-    tail = Tail(0, 0)
+    instructions = decode_instructions(path)
+    head = construct_rope(2)
+    while instructions:
+        instruction = instructions.popleft()
+        head.move(instruction)
+        if gettrace():
+            debug_print(head)
+
+    return len(head.cells_visited())
 
 
-def pt2(raw_input):
+def debug_print(head: Knot):
+
+    positions = {}
+    current_knot = head
+    knot_count = 0
+    while current_knot:
+        if current_knot.position not in positions:
+            positions[current_knot.position] = (
+                "H" if knot_count == 0 else str(knot_count)
+            )
+        current_knot = current_knot.child
+        knot_count += 1
+    if (origin := Vector2D(0, 0)) not in positions:
+        positions[origin] = "S"
+
+    for y in range(
+        max([pos.y for pos in positions]), min([pos.y for pos in positions]) - 1, -1
+    ):
+        line_str = ""
+        for x in range(
+            min([pos.x for pos in positions]), max([pos.x for pos in positions]) + 1
+        ):
+            char = positions.get(Vector2D(x, y), ".")
+            line_str += char
+
+        print(line_str)
+
+    print("\n-----\n")
+
+
+def pt2(path: Path):
     """part 2"""
+
+    instructions = decode_instructions(path)
+
+    head = construct_rope(10)
+    while instructions:
+        instruction = instructions.popleft()
+        head.move(instruction)
+        if gettrace():
+            debug_print(head)
+
+    return len(head.cells_visited())
--- 2022/day09/test_solution.py
+++ 2022/day09/test_solution.py
@@ -8,6 +8,7 @@ class TestCase(AOCTest):
     """Test case for Day 1"""
     ex = "2022/day09/example.txt"
     in_path = "2022/day09/input.txt"
+    larger_ex = "2022/day09/larger_example.txt"
 
     def test_example_part_1(self):
         """Input example data to part 1"""
@@ -16,23 +17,29 @@ class TestCase(AOCTest):
             13,
         )
 
-    # def test_example_part_2(self):
-    #     """Input example data to part 2"""
-    #     self.assertEqual(
-    #         pt2(Path(self.ex).read_text(encoding="utf8")),
-    #         ,
-    #     )
+    def test_example_part_2(self):
+        """Input example data to part 2"""
+        self.assertEqual(
+            pt2(Path(self.ex)),
+            1,
+        )
+
+    def test_example_part_2_larger(self):
+        self.assertEqual(
+            pt2(Path(self.larger_ex)),
+            36,
+        )
 
     def test_input_part_1(self):
         """Test Created with answer to allow for refactoring."""
         self.assertEqual(
-            pt1(Path(self.in_path).read_text(encoding="utf8")),
-            0,
+            pt1(Path(self.in_path)),
+            6190,
         )
 
-    # def test_input_part_2(self):
-    #     """Test Created with answer to allow refactoring"""
-    #     self.assertEqual(
-    #         pt2(Path(self.in_path).read_text(encoding="utf8")),
-    #         2639,
-    #     )
+    def test_input_part_2(self):
+        """Test Created with answer to allow refactoring"""
+        self.assertEqual(
+            pt2(Path(self.in_path)),
+            2516,
+        )
