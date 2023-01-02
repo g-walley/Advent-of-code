3a99d2459589460b9777a83e754d42078a4c9ffa
Gareth Walley <gareth@walley.me>
2023-01-02 09:47:01 +0000

    Part1 day 17

--- 2022/day17/solution.py
+++ 2022/day17/solution.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
-from dataclasses import dataclass, field
+from copy import deepcopy
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Dict, List, Set
+from collections import deque
 
 
 shapes = [
@@ -46,41 +48,161 @@ class Rock:
         self.parts = sorted(self.parts, key=lambda x: x.x)
 
         self.left = 2
-        self.right = x
+        self.right = max([part.x for part in self.parts])
 
-    def blow(self, direction: str):
-        if not self.stationary:
-            if direction == "<":
-                if self.left > 0:
-                    ...
-                    self.left -= 1
-            elif direction == ">":
-                if self.right < 6:
-                    ...
-                    self.right += 1
-            else:
-                assert False, "Direction Invalid!"
+    def move(self, direction: Point2D) -> List[Point2D]:
+        new_parts = []
+        for part in self.parts:
+            new_parts.append(part + direction)
+        return new_parts
+
+    def blow(self, direction: str) -> List[Point2D]:
+        assert direction == "<" or direction == ">", "Direction Invalid!"
+        return self.move(
+            direction=Point2D(-1, 0) if direction == "<" else Point2D(1, 0)
+        )
+
+    def drop(self) -> List[Point2D]:
+        return self.move(direction=Point2D(0, -1))
 
     @property
     def parts_sorted_bottom_up(self):
         return sorted(self.parts, key=lambda x: x.y)
 
-    # def __add__(self, other: Point2D):
-    #     return Point2D(self.x + other.x, self.y + other.y)
 
-    # def __sub__(self, other: Point2D):
-    #     return Point2D(self.x - other.x, self.y - other.y)
+def rock_shape_generator():
+    d = deque(shapes)
+    while True:
+        d.append(next := d.popleft())
+        yield next
+
+def wind_generator(raw_input: Path) -> str:
+    d = deque(list(raw_input.read_text(encoding='utf-8')))
+    while True:
+        d.append(next := d.popleft())
+        yield next
+
+
+def collision(rock_positions: List[Point2D], world: Set[Point2D]) -> bool:
+    return True if set(rock_positions).intersection(world) else False
+
+
+def print_world(static_points, rock: Rock):
+    world = "\t|-------|\n\n"
+    all_y = [p.y for p in static_points]
+    all_y.extend([r_p.y for r_p in rock.parts])
+    max_y = max(all_y)
+    for y in range(0, max_y + 1):
+        row = ""
+        for x in range(LEFT_LIMIT, RIGHT_LIMIT + 1):
+            if (p := Point2D(x, y)) in static_points:
+                row += "#"
+            elif p in rock.parts:
+                row += "@"
+            else:
+                row += "."
+        world = f"{y}\t|{row}|\n{world}"
+    print(world)
 
 
 def pt1(raw_input: Path):
     """part 1"""
-    print("\n\n")
-    print(shapes)
+    rock_gen = rock_shape_generator()
+    wind_gen = wind_generator(raw_input)
 
-    r = Rock(shape=shapes[1], bottom_row=2)
-    print(r.parts_sorted_bottom_up)
-    print("\n\n")
+    static_points: Set[Point2D] = set()
+
+    top = -1
+    for i in range(2022):
+        r = Rock(shape=next(rock_gen), bottom_row=top + 4)
+        # print("New Rock")
+        # print_world(static_points, r)
+
+        falling = True
+        while falling:
+            new_r_position = r.blow(d:= next(wind_gen))
+            # print(f"Wind: {d}")
+            out_of_bounds = any([part.x < LEFT_LIMIT or RIGHT_LIMIT < part.x for part in new_r_position])
+            if not out_of_bounds:
+                if not collision(new_r_position, static_points):
+                    r.parts = new_r_position
+
+            # print_world(static_points, r)
+
+            new_r_position = r.drop()
+            # print("Drop 1")
+            out_of_bounds = any([part.y < 0 for part in new_r_position])
+            if out_of_bounds or collision(new_r_position, static_points):
+                for point in r.parts:
+                    static_points.add(point)
+
+                falling = False
+            else:
+                r.parts = new_r_position
+
+            # print_world(static_points, r)
+
+        y_to_cmp = [top]
+        y_to_cmp.extend([part.y for part in r.parts])
+        top = max(y_to_cmp)
+        print(f"Top: {top}")
+
+    return(top + 1)
 
 
 def pt2(raw_input):
     """part 2"""
+    start_wind = deque(list(raw_input.read_text(encoding='utf-8')))
+    winds = deepcopy(start_wind)
+
+    start_rocks = deque(shapes)
+    rocks = deepcopy(start_rocks)
+
+    static_points: Set[Point2D] = set()
+
+    top = -1
+    for i in range(1000000000000):
+
+        if winds == start_wind and rocks == start_rocks and i != 0:
+            print_world(start_rocks)
+
+        shape = rocks.popleft()
+        rocks.append(shape)
+
+        r = Rock(shape=shape, bottom_row=top + 4)
+        # print("New Rock")
+        # print_world(static_points, r)
+
+        falling = True
+        while falling:
+            wind_dir = winds.popleft()
+            winds.append(wind_dir)
+            new_r_position = r.blow(wind_dir)
+            # print(f"Wind: {wind_dir}")
+            out_of_bounds = any([part.x < LEFT_LIMIT or RIGHT_LIMIT < part.x for part in new_r_position])
+            if not out_of_bounds:
+                if not collision(new_r_position, static_points):
+                    r.parts = new_r_position
+
+            # print_world(static_points, r)
+
+            new_r_position = r.drop()
+            # print("Drop 1")
+            out_of_bounds = any([part.y < 0 for part in new_r_position])
+            if out_of_bounds or collision(new_r_position, static_points):
+                for point in r.parts:
+                    static_points.add(point)
+
+                falling = False
+            else:
+                r.parts = new_r_position
+
+            # print_world(static_points, r)
+
+        y_to_cmp = [top]
+        y_to_cmp.extend([part.y for part in r.parts])
+        top = max(y_to_cmp)
+        if i % 100000 == 0:
+            print(f"Shape {i} | Top: {top}")
+
+    return(top + 1)
--- 2022/day17/test_solution.py
+++ 2022/day17/test_solution.py
@@ -15,23 +15,23 @@ class TestCase(AOCTest):
         """Input example data to part 1"""
         self.assertEqual(pt1(Path(self.ex)), 3068)
 
-    # def test_example_part_2(self):
-    #     """Input example data to part 2"""
-    #     self.assertEqual(
-    #         pt2(Path(self.ex)),
-    #         ,
-    #     )
+    def test_example_part_2(self):
+        """Input example data to part 2"""
+        self.assertEqual(
+            pt2(Path(self.ex)),
+            1514285714288,
+        )
 
-    # def test_input_part_1(self):
-    #     """Test Created with answer to allow for refactoring."""
-    #     self.assertEqual(
-    #         pt1(Path(self.in_path)),
-    #         7811,
-    #     )
+    def test_input_part_1(self):
+        """Test Created with answer to allow for refactoring."""
+        self.assertEqual(
+            pt1(Path(self.in_path)),
+            3055,
+        )
 
-    # def test_input_part_2(self):
-    #     """Test Created with answer to allow refactoring"""
-    #     self.assertEqual(
-    #         pt2(Path(self.in_path)),
-    #         2639,
-    #     )
+    def test_input_part_2(self):
+        """Test Created with answer to allow refactoring"""
+        self.assertEqual(
+            pt2(Path(self.in_path)),
+            0,
+        )
