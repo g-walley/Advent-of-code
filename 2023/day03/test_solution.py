1e94d0cb64f5da833af43c87260a7e2c00e264ac
Gareth Walley <gareth@walley.me>
2023-12-03 11:26:28 +0000

    Pt1 passing

--- 2023/day03/test_solution.py
+++ 2023/day03/test_solution.py
@@ -4,17 +4,16 @@ from pathlib import Path
 from shared_test import AOCTest
 from day03.solution import pt1, pt2
 
+
 class TestCase(AOCTest):
     """Test case for Day 1"""
+
     ex = "2023/day03/example.txt"
     in_path = "2023/day03/input.txt"
 
     def test_example_part_1(self):
         """Input example data to part 1"""
-        self.assertEqual(
-            pt1(Path(self.ex)),
-            4361
-        )
+        self.assertEqual(pt1(Path(self.ex)), 4361)
 
     # def test_example_part_2(self):
     #     """Input example data to part 2"""
@@ -23,12 +22,12 @@ class TestCase(AOCTest):
     #         ,
     #     )
 
-    # def test_input_part_1(self):
-    #     """Test Created with answer to allow for refactoring."""
-    #     self.assertEqual(
-    #         pt1(Path(self.in_path)),
-    #         7811,
-    #     )
+    def test_input_part_1(self):
+        """Test Created with answer to allow for refactoring."""
+        self.assertEqual(
+            pt1(Path(self.in_path)),
+            528819,
+        )
 
     # def test_input_part_2(self):
     #     """Test Created with answer to allow refactoring"""
